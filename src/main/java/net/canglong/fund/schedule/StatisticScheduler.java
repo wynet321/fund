3d612bcb18cb0d53ec4f08ef24b626714a7deb33
Yi Wang <yi.wang@zuora.com>
2025-10-25 17:32:10 +0800

    upgrade to latest spring

--- src/main/java/net/canglong/fund/schedule/StatisticScheduler.java
+++ src/main/java/net/canglong/fund/schedule/StatisticScheduler.java
@@ -13,28 +13,34 @@ import org.springframework.stereotype.Component;
 public class StatisticScheduler {
 
   private final RateService rateService;
+  private boolean monitorRequired;
 
   public StatisticScheduler(RateService rateService) {
     this.rateService = rateService;
+    monitorRequired = false;
   }
 
   // Run roughly daily
   @Scheduled(fixedDelay = 108000000)
   public void runStatisticGeneration() {
     // Generate statistics for default types without refreshing all data
+    monitorRequired = true;
     rateService.generateStatisticData(List.of("混合型", "股票型", "债券型", "QDII", "短期理财债券型"), false);
   }
 
   // Report job status periodically
   @Scheduled(fixedDelay = 60000)
   public void reportStatisticJobStatus() {
-    Status status = rateService.getStatisticJobStatus();
-    if (status.isTerminated()) {
-      log.info("\n*******************\nStatistic job was completed.\n*******************");
-    } else if (status.getAliveThreadCount() > 0) {
-      log.info(
-          "\n*******************\nStatistic generation\nLeft fund count: {}\nElapse Time: {}\nActive Threads: {}\n*******************",
-          status.getLeftFundCount(), status.getElapseTime(), status.getAliveThreadCount());
+    if (monitorRequired) {
+      Status status = rateService.getStatisticJobStatus();
+      if (status.isTerminated()) {
+        log.info("\n*******************\nStatistic job was completed.\n*******************");
+        monitorRequired = false;
+      } else if (status.getAliveThreadCount() > 0) {
+        log.info(
+            "\n*******************\nStatistic generation\nLeft fund count: {}\nElapse Time: {}\nActive Threads: {}\n*******************",
+            status.getLeftFundCount(), status.getElapseTime(), status.getAliveThreadCount());
+      }
     }
   }
 }
